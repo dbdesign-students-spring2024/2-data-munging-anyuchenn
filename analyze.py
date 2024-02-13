1930b90a4370c78c9e90d2d1aa95d328f1d018f5
anyuchenn <ac9250@nyu.edu>
2024-02-13 01:27:15 -0500

    assignment 2(Anyu&Yuhao)

--- analyze.py
+++ analyze.py
@@ -1,1 +1,24 @@
-# Place code below to do the analysis part of the assignment.
+def averages():
+    with open("/Users/yuyu/Desktop/2-data-munging-anyuchenn/data/clean_data.csv", "r") as file:
+        next(file)  
+        
+        data = []
+        for line in file:
+            parts = line.strip().split(',')
+            data.append(float(parts[1]))  
+
+    start = 1880
+    sum_temp, count = 0, 0
+
+    for i, temp in enumerate(data):
+        sum_temp += temp
+        count += 1
+
+        if (i + 1) % 10 == 0 or i + 1 == len(data):
+            avg_temp = sum_temp / count
+            print(f"{start} to {start + 9}: {round(avg_temp, 2)}")
+            
+            start += 10
+            sum_temp, count = 0, 0
+
+averages()
--- munge.py
+++ munge.py
@@ -1,1 +1,41 @@
-# Place code below to do the munging part of this assignment.
+orig = open("/Users/yuyu/Desktop/2-data-munging-anyuchenn/data/GLB.Ts+dSST.txt", 'r')
+clean = open("/Users/yuyu/Desktop/2-data-munging-anyuchenn/data/clean_data.csv", 'w')
+
+def fix_nums(ns):
+    fixed_ns = []
+    for n in ns:
+        try:
+            n_float = float(n)  
+            if n_float < 1879:
+                res = round((n_float / 100) * 1.8, 1)  
+                fixed_ns.append(str(res))  
+            else:
+                fixed_ns.append(n)  
+        except ValueError:
+            fixed_ns.append(n)  
+    return fixed_ns
+
+
+
+for _ in range(7):
+    orig.readline()
+
+hdr = orig.readline().strip()
+hdr_cols = ','.join(hdr.split())
+clean.write(hdr_cols + "\n")
+
+lines = orig.readlines()
+lines = lines[:-7]
+
+for idx, line in enumerate(lines):
+    if line.strip():
+        parts = line.split()
+        parts_fixed = fix_nums(parts)
+        line_new = ','.join(parts_fixed)
+        if line_new != hdr_cols:
+            clean.write(line_new)
+            if idx < len(lines) - 1:
+                clean.write("\n")
+
+orig.close()
+clean.close()
